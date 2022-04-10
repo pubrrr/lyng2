06b58920f5e090c08a7e5d5e91295f093a43cfaf
pubrrr <pubr@posteo.de>
2022-04-10 18:10:54 +0200

    make Elm and Rust talk via a websocket

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,5 +1,56 @@
-module Main exposing (main)
+port module Main exposing (Msg(..), main, update, view)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Json.Encode exposing (Value, string)
 
-main = text "hi"
+
+port sendMessage : Value -> Cmd msg
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+type alias Model =
+    Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, sendMessage (string "increment") )
+
+        Decrement ->
+            ( model - 1, sendMessage (string "decrement") )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( 0, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
