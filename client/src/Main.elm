880521a521cbf424409f7457133386eb9d33b492
pubrrr <pubr@posteo.de>
2022-04-19 18:53:47 +0200

    two boxes to write / display text

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -2,7 +2,9 @@ port module Main exposing (Msg(..), main, update, view)
 
 import Browser
 import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, contenteditable)
+import Html.Events exposing (on, onClick)
+import Json.Decode
 import Json.Encode exposing (Value, string)
 
 
@@ -21,6 +23,7 @@ main =
 type Msg
     = Increment
     | Decrement
+    | Something Value
 
 
 type alias Model =
@@ -36,6 +39,9 @@ update msg model =
         Decrement ->
             ( model - 1, sendMessage (string "decrement") )
 
+        Something value ->
+            ( model - 1, sendMessage value )
+
 
 view : Model -> Html Msg
 view model =
@@ -43,6 +49,10 @@ view model =
         [ button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment ] [ text "+" ]
+        , div [ class "editorContainer" ]
+            [ div [ contenteditable True, class "editorWindow", on "input" (Json.Decode.value |> Json.Decode.map (\value -> Something value)) ] [ text "" ]
+            , div [ class "editorWindow" ] [ text "result..." ]
+            ]
         ]
 
 
