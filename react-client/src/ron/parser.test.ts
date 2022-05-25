5a2885d888ae2a336a56f6b9f6a0ac837af04da3
pubrrr <pubr@posteo.de>
2022-05-25 23:29:17 +0200

    decode strings, sketch how to decode tuple structs

--- react-client/src/ron/parser.test.ts
+++ react-client/src/ron/parser.test.ts
@@ -1,4 +1,10 @@
-import { EnumVariant, TupleStruct } from "./parser";
+import {
+    decode,
+    EnumVariant,
+    string,
+    tupleStruct,
+    TupleStruct,
+} from "./parser";
 import {
     apply,
     buildLexer,
@@ -21,7 +27,7 @@ enum TokenKind {
     DoubleQuote,
 }
 
-function decode(input: string): TupleStruct<"Success", [string]> {
+function decode2(input: string): TupleStruct<"Success", [string]> {
     const lexer = buildLexer([
         [true, /^[a-zA-Z][a-zA-Z\d]*/g, TokenKind.Identifier],
         [true, /^[^")(]*/g, TokenKind.String],
@@ -51,7 +57,56 @@ function decode(input: string): TupleStruct<"Success", [string]> {
 }
 
 test("tuple with one field", () => {
-    let result = decode('Success("success message")');
+    let result = decode2('Success("success message")');
 
     expect(result.value).toEqual(["success message"]);
+});
+
+describe("decoding RON strings", () => {
+    test("should succeed on a valid string", () => {
+        let result = decode('"valid string"', string);
+
+        expect(result.success).toBe(true);
+        expect((result as { value: string }).value).toBe("valid string");
+    });
+
+    test("should fail with missing quotes", () => {
+        let result = decode("has no quotes", string);
+
+        expect(result.success).toBe(false);
+        expect((result as { error: string }).error).toBe(
+            "expected opening double quotes"
+        );
+    });
+
+    test("should fail with missing closing double quotes", () => {
+        let result = decode('"has no closing quotes', string);
+
+        expect(result.success).toBe(false);
+        expect((result as { error: string }).error).toBe(
+            "expected closing double quotes"
+        );
+    });
+
+    test("should fail on double quotes in the middle", () => {
+        let result = decode('"misplaced " quote"', string);
+
+        expect(result.success).toBe(false);
+    });
+});
+
+describe("decoding RON tuple structs", () => {
+    test("should decode a valid tuple struct with one string field", () => {
+        let underTest = tupleStruct("Success", [string]);
+
+        let result = decode('Success("success message")', underTest);
+
+        expect(result.success).toBe(true);
+        expect(
+            (result as { value: TupleStruct<"Success", string[]> }).value
+        ).toEqual({
+            name: "Success",
+            value: ["success message"],
+        });
+    });
 });
--- react-client/src/ron/parser.ts
+++ react-client/src/ron/parser.ts
@@ -1,38 +1,136 @@
-export type StringLiteral<T extends string> = string extends T ? never : T;
+import {
+    apply,
+    buildLexer,
+    err,
+    expectEOF,
+    kmid,
+    Parser,
+    ParserOutput,
+    seq,
+    str,
+    tok,
+    Token,
+} from "typescript-parsec";
 
-export interface EnumVariant<Literal extends string, Value> {
-    name: StringLiteral<Literal>;
-    value: Value;
+enum RonToken {
+    Identifier,
+    String,
+    LeftParenthesis,
+    RightParenthesis,
+    DoubleQuote,
 }
+
+const RON_LEXER = buildLexer([
+    [true, /^[a-zA-Z][a-zA-Z\d]*/g, RonToken.Identifier],
+    [true, /^[^")(]*/g, RonToken.String],
+    [true, /^\(/g, RonToken.LeftParenthesis],
+    [true, /^\)/g, RonToken.RightParenthesis],
+    [true, /^"/g, RonToken.DoubleQuote],
+]);
+
+interface RonDecoder<V> {
+    (token: Token<RonToken> | undefined): ParserOutput<RonToken, V>;
+}
+
+export type Result<V> =
+    | {
+          success: true;
+          value: V;
+      }
+    | {
+          success: false;
+          error: string;
+      };
+
+export function decode<V>(input: string, decoder: RonDecoder<V>): Result<V> {
+    const parseResult = expectEOF(decoder(RON_LEXER.parse(input)));
+
+    if (!parseResult.successful) {
+        return { success: false, error: parseResult.error.message };
+    }
+    if (parseResult.candidates.length == 0) {
+        return {
+            success: false,
+            error: "Decoding RON value returned no result",
+        };
+    }
+    if (parseResult.candidates.length > 1) {
+        return {
+            success: false,
+            error: "Decoding RON value returned ambiguous values",
+        };
+    }
+    return { success: true, value: parseResult.candidates[0].result };
+}
+
+export const string = kmid(
+    err(tok(RonToken.DoubleQuote), "expected opening double quotes"),
+    apply(tok(RonToken.String), (token: Token<RonToken>): string => token.text),
+    err(tok(RonToken.DoubleQuote), "expected closing double quotes")
+).parse;
+
+export function tupleStruct<Literal extends string, Value extends any[]>(
+    name: StringLiteral<Literal>,
+    fields: FieldsArray<Value>
+): RonDecoder<TupleStruct<Literal, Value>> {
+    return (
+        token: Token<RonToken> | undefined
+    ): ParserOutput<RonToken, TupleStruct<Literal, Value>> => {
+        let parser = apply(
+            seq(
+                str<RonToken>(name),
+                tok(RonToken.LeftParenthesis),
+                fieldsParser(fields),
+                tok(RonToken.RightParenthesis)
+            ),
+            (value) => {
+                return {
+                    name,
+                    value: value[2],
+                };
+            }
+        );
+        return parser.parse(token);
+    };
+}
+
+function fieldsParser<Value extends any[]>(
+    fields: FieldsArray<Value>
+): Parser<RonToken, Value> {
+    function parse(
+        token: Token<RonToken> | undefined
+    ): ParserOutput<RonToken, Value> {
+        let [first, ...rest] = fields;
+
+        let result = apply(
+            { parse: first as RonDecoder<Value[0]> },
+            (value) => [value]
+        ).parse(token);
+        if (!result.successful) {
+            return result;
+        }
+
+        if (rest.length == 0) {
+            return result as ParserOutput<RonToken, Value>;
+        }
+        throw "ohoh";
+    }
+
+    return { parse };
+}
+
+type FieldsArray<V extends any[]> = {
+    [Key in keyof V]: RonDecoder<V[Key]>;
+};
 
 export type TupleStruct<Literal extends string, Value extends any[]> = {
     name: StringLiteral<Literal>;
     value: Value;
 };
 
-export interface RonDecoder<V> {
-    (string: string): V;
+export type StringLiteral<T extends string> = string extends T ? never : T;
+
+export interface EnumVariant<Literal extends string, Value> {
+    name: StringLiteral<Literal>;
+    value: Value;
 }
-
-class TupleStruct2<V extends any[]> implements RonDecoder<V> {
-    // readonly name: String;
-
-    constructor(name: String) {
-        // this.name = name;
-    }
-
-    parse(string: string): V {
-        throw "unimplemented";
-    }
-}
-
-export function tupleStruct<V extends any[]>(
-    name: String,
-    fields: FieldsArray<V>
-): TupleStruct2<V> {
-    return new TupleStruct2<V>(name);
-}
-
-type FieldsArray<V extends any[]> = {
-    [Key in keyof V]: RonDecoder<V[Key]>;
-};
