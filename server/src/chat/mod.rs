3c47d9cb4611c8e1c9fef6a3a9b11a047f3a471e
pubrrr <pubr@posteo.de>
2022-11-17 23:23:52 +0100

    implement sending chat messages to the API

--- server/src/chat/mod.rs
+++ server/src/chat/mod.rs
@@ -3,6 +3,7 @@ use std::sync::Mutex;
 use async_graphql::async_stream::stream;
 use async_graphql::extensions::Logger;
 use async_graphql::{Context, Object, SimpleObject, Subscription};
+use chrono::{DateTime, Local};
 use futures_util::Stream;
 use log::{debug, info};
 use tokio::sync::mpsc;
@@ -12,9 +13,11 @@ use warp::http::header::SET_COOKIE;
 use crate::chat::auth::{create_auth_token, AuthExtensionFactory, AuthUser, AUTH_COOKIE_NAME};
 
 pub mod auth;
+#[cfg(test)]
+mod test;
 
 type Users = Mutex<Vec<User>>;
-type Streams = Mutex<Vec<UnboundedSender<User>>>;
+type Streams<T> = Mutex<Vec<UnboundedSender<T>>>;
 
 pub type Schema = async_graphql::Schema<Query, Mutation, Subscription>;
 
@@ -24,10 +27,18 @@ pub struct User {
     name: String,
 }
 
+#[derive(Clone, Debug, SimpleObject)]
+pub struct Message {
+    user: User,
+    message: String,
+    date: DateTime<Local>,
+}
+
 pub fn build_schema() -> Schema {
     Schema::build(Query, Mutation, Subscription)
         .data(Users::default())
-        .data(Streams::default())
+        .data(Streams::<User>::default())
+        .data(Streams::<Message>::default())
         .extension(Logger)
         .extension(AuthExtensionFactory)
         .finish()
@@ -60,7 +71,7 @@ impl Mutation {
         };
         users.push(new_user.clone());
 
-        let mut subscribers = ctx.data_unchecked::<Streams>().lock().unwrap();
+        let mut subscribers = ctx.data_unchecked::<Streams<User>>().lock().unwrap();
         notify_subscribers(new_user.clone(), &mut subscribers);
         info!("new user registered: {new_user:?}");
 
@@ -72,12 +83,40 @@ impl Mutation {
 
         new_user
     }
+
+    async fn send_message(&self, ctx: &Context<'_>, message: String) -> Message {
+        info!("new message received: {message}");
+        println!("new message received: {message}");
+        let mut subscribers = ctx.data_unchecked::<Streams<Message>>().lock().unwrap();
+
+        let user = get_user(ctx);
+        info!("from user: {user:?}");
+        println!("from user: {user:?}");
+
+        let message = Message {
+            user,
+            message,
+            date: Local::now(),
+        };
+        notify_subscribers(message.clone(), &mut subscribers);
+        message
+    }
 }
 
-fn notify_subscribers(new_user: User, subscribers: &mut Vec<UnboundedSender<User>>) {
+fn get_user(ctx: &Context) -> User {
+    let auth_user = ctx.data_unchecked::<AuthUser>();
+    let users = ctx.data_unchecked::<Users>().lock().unwrap();
+    users
+        .iter()
+        .find(|user| user.id == auth_user.id)
+        .unwrap()
+        .clone()
+}
+
+fn notify_subscribers<T: Clone>(message: T, subscribers: &mut Vec<UnboundedSender<T>>) {
     for (i, stream) in subscribers.clone().iter().enumerate() {
         debug!("sending new user");
-        match stream.send(new_user.clone()) {
+        match stream.send(message.clone()) {
             Err(_) if stream.is_closed() => {
                 debug!("stream disconnected - removing it");
                 subscribers.remove(i);
@@ -92,11 +131,27 @@ pub struct Subscription;
 #[Subscription]
 impl Subscription {
     async fn get_new_users(&self, ctx: &Context<'_>) -> impl Stream<Item = User> {
-        info!("new subscription");
+        info!("new subscription for users");
 
         let (sender, mut receiver) = mpsc::unbounded_channel::<User>();
 
-        let mut streams = ctx.data_unchecked::<Streams>().lock().unwrap();
+        let mut streams = ctx.data_unchecked::<Streams<User>>().lock().unwrap();
+        streams.push(sender);
+
+        stream! {
+            while let Some(item) = receiver.recv().await {
+                yield item;
+            }
+        }
+    }
+
+    async fn get_new_messages(&self, ctx: &Context<'_>) -> impl Stream<Item = Message> {
+        info!("new subscription for messages");
+        println!("new subscription for messages");
+
+        let (sender, mut receiver) = mpsc::unbounded_channel::<Message>();
+
+        let mut streams = ctx.data_unchecked::<Streams<Message>>().lock().unwrap();
         streams.push(sender);
 
         stream! {
---
+++ server/src/chat/test.rs
@@ -0,0 +1,236 @@
+use std::fmt::Debug;
+
+use async_graphql::Value::Null;
+use async_graphql::{Request, Response, ServerError, Value};
+use futures_lite::future::poll_once;
+use futures_util::{Stream, StreamExt};
+
+use crate::chat::auth::AuthUser;
+use crate::chat::{build_schema, Schema};
+use crate::from_json;
+
+#[tokio::test]
+async fn test_register_user() {
+    let schema = build_schema();
+
+    let response = register_user(&schema).await;
+
+    assert_no_error(&response);
+    assert_eq!(
+        from_json!({"register": {"id": "User#0", "name": "user name"}}),
+        response.data,
+    );
+}
+
+#[tokio::test]
+async fn get_users_without_authentication_fails() {
+    let schema = build_schema();
+
+    let response = schema.execute("{ getUsers { id } }").await;
+
+    assert_missing_authentication(response);
+}
+
+#[tokio::test]
+async fn get_users_with_authentication_returns_registered_users() {
+    let schema = build_schema();
+    register_user(&schema).await;
+    register_user(&schema).await;
+
+    let request = Request::from("{ getUsers { id } }").data(some_auth_user());
+    let response = schema.execute(request).await;
+
+    assert_no_error(&response);
+    assert_eq!(
+        from_json!({"getUsers": [{"id": "User#0"}, {"id": "User#1"}]}),
+        response.data,
+    );
+}
+
+#[tokio::test]
+async fn get_logged_in_user_with_authentication_returns_user() {
+    let schema = build_schema();
+    register_user(&schema).await;
+
+    let request = Request::from("{ loggedInUser { id } }").data(some_auth_user());
+    let response: Response = schema.execute(request).await;
+
+    assert_no_error(&response);
+    assert_eq!(
+        from_json!({"loggedInUser": {"id": "User#0"}}),
+        response.data,
+    );
+}
+
+#[tokio::test]
+async fn get_logged_in_user_without_authentication_returns_no_data() {
+    let schema = build_schema();
+    register_user(&schema).await;
+
+    let request = Request::from("{ loggedInUser { id } }");
+    let response: Response = schema.execute(request).await;
+
+    assert_no_error(&response);
+    assert_eq!(from_json!({ "loggedInUser": null }), response.data,);
+}
+
+mod subscribe_to_new_users {
+    use super::*;
+
+    #[tokio::test]
+    async fn get_new_users_with_authentication() {
+        let schema = build_schema();
+
+        let request =
+            Request::from("subscription { getNewUsers { id, name } }").data(some_auth_user());
+        let mut stream = schema.execute_stream(request);
+        poll_once_to_make_stream_perform_work(&mut stream).await;
+
+        let response = register_user(&schema).await;
+        assert_no_error(&response);
+
+        let response: Response = stream.next().await.unwrap();
+
+        assert_no_error(&response);
+        assert_eq!(
+            from_json!({ "getNewUsers": {"id": "User#0", "name": "user name"} }),
+            response.data,
+        );
+    }
+
+    #[tokio::test]
+    async fn get_new_users_without_authentication_denies_access() {
+        let schema = build_schema();
+
+        let mut stream = schema.execute_stream("subscription { getNewUsers { id, name } }");
+        let response = stream.next().await.unwrap();
+
+        assert_missing_authentication(response);
+    }
+}
+
+mod send_messages {
+    use std::str::FromStr;
+
+    use async_graphql::Value::{Object, String};
+    use chrono::{DateTime, Local};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn send_message_without_authentication_fails() {
+        let schema = build_schema();
+
+        let response = schema
+            .execute("mutation { sendMessage(message: \"test message\") }")
+            .await;
+
+        assert_missing_authentication(response);
+    }
+
+    #[tokio::test]
+    async fn subscribe_to_messages_without_authentication_fails() {
+        let schema = build_schema();
+
+        let mut stream = schema.execute_stream("subscription { getNewMessages }");
+        let response = stream.next().await.unwrap();
+
+        assert_missing_authentication(response);
+    }
+
+    #[tokio::test]
+    async fn subscribe_to_messages_receives_sent_messages() {
+        let schema = build_schema();
+        register_user(&schema).await;
+
+        let request =
+            Request::from("subscription { getNewMessages { message,  user { id, name } } }")
+                .data(some_auth_user());
+        let mut stream = schema.execute_stream(request);
+        poll_once_to_make_stream_perform_work(&mut stream).await;
+
+        let request =
+            Request::from("mutation { sendMessage(message: \"test message\") { message } }")
+                .data(some_auth_user());
+        let sent_message = schema.execute(request).await;
+        assert_no_error(&sent_message);
+
+        let response: Response = stream.next().await.unwrap();
+
+        assert_no_error(&response);
+        assert_eq!(
+            from_json!({ "getNewMessages": {
+                "user": {"id": "User#0", "name": "user name"},
+                "message": "test message"
+            }}),
+            response.data,
+        );
+    }
+
+    #[tokio::test]
+    async fn date_of_sent_message() {
+        let schema = build_schema();
+        register_user(&schema).await;
+
+        let request = Request::from("mutation { sendMessage(message: \"test message\") { date } }")
+            .data(some_auth_user());
+        let sent_message = schema.execute(request).await;
+        assert_no_error(&sent_message);
+        match &sent_message.data {
+            Object(send_message) => match &send_message["sendMessage"] {
+                Object(message) => {
+                    if let String(date) = &message["date"] {
+                        DateTime::<Local>::from_str(date).unwrap();
+                        return;
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+        panic!("Did not find expected date in {:?}", sent_message.data);
+    }
+}
+
+async fn poll_once_to_make_stream_perform_work<
+    I: PartialEq + Debug,
+    S: Stream<Item = I> + Unpin,
+>(
+    stream: &mut S,
+) -> Option<Option<S::Item>> {
+    let response = poll_once(stream.next()).await;
+    assert_eq!(None, response, "{response:?}");
+    response
+}
+
+fn assert_no_error(response: &Response) {
+    assert_eq!(0, response.errors.len(), "{:?}", response.errors)
+}
+
+fn assert_missing_authentication(response: Response) {
+    assert_eq!(Null, response.data);
+    assert_eq!(1, response.errors.len());
+    assert_eq!(
+        ServerError::new("Missing authentication for operation", None),
+        response.errors[0]
+    )
+}
+
+async fn register_user(schema: &Schema) -> Response {
+    schema
+        .execute("mutation { register(name:\"user name\") { id, name } }")
+        .await
+}
+
+fn some_auth_user() -> AuthUser {
+    AuthUser {
+        id: "User#0".to_string(),
+    }
+}
+
+#[macro_export]
+macro_rules! from_json {
+    ($($json:tt)+) => {
+        Value::from_json(serde_json::json!($($json)+)).unwrap()
+    };
+}
